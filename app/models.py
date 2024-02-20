cffc2269834275eb9907e670fd9e83afe66b3971
JKimani77 <jkkimani77@gmail.com>
2024-02-20 14:59:11 +0300

    created db models as classes for reviews and grocery list

--- app/models.py
+++ app/models.py
@@ -12,4 +12,19 @@ class User:
         self.username = username
         self.password = password
 
-        
+class Review:
+    def __init__(self, id, header,recipeid, description, rating) -> None:
+        self.id = id
+        self.header = header
+        self.recipeid = recipeid
+        self.description = description
+        self.rating = rating
+
+class GroceryList:
+    def __init__(self,id, itemname, quantity, recipeid) -> None:
+            self.id = id
+            self.itemname = itemname
+            self.quantity = quantity
+            self.recipeid = recipeid
+            
+
