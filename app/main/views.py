6dd4f4f958776da647860a6218c0498fabb71749
JKimani77 <jkkimani77@gmail.com>
2024-02-20 15:49:23 +0300

    Added new template files and html code

--- app/main/views.py
+++ app/main/views.py
@@ -2,5 +2,5 @@ from flask import render_template
 from . import main
 
 @main.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('index.html')
