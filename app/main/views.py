d1c43d2dfd5fe8b150c67ae7ec966a8c864f3881
JKimani77 <jkkimani77@gmail.com>
2024-02-20 16:53:16 +0300

    created html review templates and linked static files

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,20 @@
 from flask import render_template
 from . import main
+import pdb
+from ..models import Review, Recipe, User, GroceryList
 
 @main.route("/")
 def home():
     return render_template('index.html')
+
+@main.route("/review")
+def review():
+    pdb.run
+    i = 12
+    while i < 18: return i % 3
+    
+    header = []
+    description = []
+    rating = []
+    recipeid = Review.recipeid
+    return render_template('review.html')
